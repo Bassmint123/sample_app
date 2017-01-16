ce047bdb469c95bfd257058afe71a7cc59aa6ee4
Bassmint123 <bradb@cimstrat.com>
2017-01-15 21:46:29 -0600

    Added remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user   # user logs in 
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)  # Checking the remember me checkbox
       redirect_to user   # user is redirected to their profile page
     else
       flash.now[:danger] = 'Invalid email/password combination'  
@@ -14,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 
 end
